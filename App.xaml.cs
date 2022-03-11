585c6b0018bccbfcb6fdadd9aa3d21d18a2c454b
Nikolai <Nikolai@LAPTOP>
2022-03-11 09:18:42 +0700

    Добавлена форма указания подписей Сгенерирована база дынных

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+
 
 namespace OP1
 {
@@ -13,5 +16,38 @@ namespace OP1
     /// </summary>
     public partial class App : Application
     {
+
+        #region Private members
+        private readonly ServiceProvider serviceProvider;
+        #endregion
+
+        #region Constructor
+        public App()
+        {
+            /*
+            ServiceCollection services = new ServiceCollection();
+
+            services.AddDbContext<OP1_DbContext>(options =>
+            {
+                options.UseSqlite("Data Source = OP1.db");
+            });
+
+            services.AddSingleton<MainWindow>();
+            serviceProvider = services.BuildServiceProvider();
+            */
+        }
+        #endregion
+
+        /*
+        #region Event Handlers
+        private void OnStartup(object s, StartupEventArgs e)
+        {
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
+        #endregion
+        */
     }
+
+
 }
---
+++ Forms/Signs.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OP1.Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для Signs.xaml
+    /// </summary>
+    public partial class Signs : Window
+    {
+        public Signs()
+        {
+            InitializeComponent();
+        }
+    }
+}
